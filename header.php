8935bcbbf6f28f7bbdec59afd01cd20dcd7a5f0a
Andy <ab@marmato.de>
2016-05-27 10:39:24 +0200

    first pages

---
+++ header.php
@@ -0,0 +1,5 @@
+	<title><?php wp_title( '|', true, 'right' ); ?><?php bloginfo( 'name' ); ?></title>
+	
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<?php wp_head(); ?>
