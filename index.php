89fc4cd69ae954207d64efc9968cc0148b00d800
Andy <ab@marmato.de>
2016-05-27 09:10:54 +0200

    refactor repository

--- index.php
+++ index.php
@@ -1,17 +1,40 @@
-<?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
+<!DOCTYPE html>
+<html>
+	<head>
+		<?php get_header(); ?>
+	</head>
+	<body>
+		<h1><a href="<?php echo esc_url( home_url( '/' ) );?>" rel="home"><?php
+		bloginfo( 'name' ); ?></a></h1>
+		<p><?php bloginfo( 'description' ); ?></p>
+		<img src="<?php bloginfo('stylesheet_directory'); ?>/logo.png alt="Logo" >
+		
+<!-- Gibt es mindestens einen Beitrag? -->
+<?php if ( have_posts() ) : ?>
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define('WP_USE_THEMES', true);
+<!-- Dann nacheinander alle Beiträge ausgeben: -->
+<?php while ( have_posts() ) : the_post(); ?>
 
-/** Loads the WordPress Environment and Template */
-require( dirname( __FILE__ ) . '/wp-blog-header.php' );
+<!-- Hier den Beitrag anzeigen -->
+<article>
+		<header>
+			 <!-- Seine Überschrift -->
+			 <h1> <a href="<?php esc_url( the_permalink() ); ?>"><?php the_title(); ?></a> </h1>
+			 <!-- Sein Autor -->
+			 Verfasst von <?php the_author_posts_link(); ?>
+			 <!-- Sein Veröffentlichungsdatum -->
+			 am <?php the_date(); ?>
+			 <!-- Die Kategorie des Beitrags -->
+			 <?php the_category(); ?>
+		</header>
+			<!-- Zeige den Inhalt an -->
+			<?php the_content(); ?>
+			<!-- Hinweis auf Kommentare -->
+			<p> <?php comments_popup_link('Kein Kommentar abgegeben', 'Ein Kommentar	vorhanden', '% Kommentare vorhanden', '', 'Kommentare geschlossen'); ?> </p>
+	</article>
+<?php endwhile; ?>
+
+ <!-- Sofern es keinen Beitrag gibt, darauf hinweisen: -->
+ <?php else : ?>
+  <p>Es gibt keine Beiträge!</p>
+<?php endif; ?>
