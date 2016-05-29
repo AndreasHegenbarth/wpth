4e08e0d30430f2920130775345adf36ffc433372
Andreas Hegenbarth <Andreas.Hegenbarth@gmx.de>
2016-05-29 16:23:20 +0200

    add navigation and footer

--- functions.php
+++ functions.php
@@ -1,1 +1,4 @@
-functions.php
+<?php
+register_nav_menu('mainmenu','Hauptmenü');
+
+?>
--- index.php
+++ index.php
@@ -8,34 +8,39 @@
 		bloginfo( 'name' ); ?></a></h1>
 		<p><?php bloginfo( 'description' ); ?></p>
 		<img src="<?php bloginfo('stylesheet_directory'); ?>/logo.png alt="Logo" >
+		<?php wp_nav_menu( array('theme_location' => 'mainmenu') );  ?>
+				
+		<!-- Gibt es mindestens einen Beitrag? -->
+		<?php if ( have_posts() ) : ?>
 
-		<?php echo ah_get_locationdata(); ?>
-<!-- Gibt es mindestens einen Beitrag? -->
-<?php if ( have_posts() ) : ?>
+		<!-- Dann nacheinander alle Beitraege ausgeben: -->
+		<?php while ( have_posts() ) : the_post(); ?>
 
-<!-- Dann nacheinander alle Beitraege ausgeben: -->
-<?php while ( have_posts() ) : the_post(); ?>
+		<!-- Hier den Beitrag anzeigen -->
+		<article>
+				<header>
+					 <!-- Seine ueberschrift -->
+					 <h1> <a href="<?php esc_url( the_permalink() ); ?>"><?php the_title(); ?></a> </h1>
+					 <!-- Sein Autor -->
+					 Verfasst von <?php the_author_posts_link(); ?>
+					 <!-- Sein Veoeffentlichungsdatum -->
+					 am <?php the_date(); ?>
+					 <!-- Die Kategorie des Beitrags -->
+					 <?php the_category(); ?>
+				</header>
+					<!-- Zeige den Inhalt an -->
+					<?php the_content(); ?>
+					<!-- Hinweis auf Kommentare -->
+					<p> <?php comments_popup_link('Kein Kommentar abgegeben', 'Ein Kommentar	vorhanden', '% Kommentare vorhanden', '', 'Kommentare geschlossen'); ?> </p>
+			</article>
+		<?php endwhile; ?>
 
-<!-- Hier den Beitrag anzeigen -->
-<article>
-		<header>
-			 <!-- Seine ueberschrift -->
-			 <h1> <a href="<?php esc_url( the_permalink() ); ?>"><?php the_title(); ?></a> </h1>
-			 <!-- Sein Autor -->
-			 Verfasst von <?php the_author_posts_link(); ?>
-			 <!-- Sein Veoeffentlichungsdatum -->
-			 am <?php the_date(); ?>
-			 <!-- Die Kategorie des Beitrags -->
-			 <?php the_category(); ?>
-		</header>
-			<!-- Zeige den Inhalt an -->
-			<?php the_content(); ?>
-			<!-- Hinweis auf Kommentare -->
-			<p> <?php comments_popup_link('Kein Kommentar abgegeben', 'Ein Kommentar	vorhanden', '% Kommentare vorhanden', '', 'Kommentare geschlossen'); ?> </p>
-	</article>
-<?php endwhile; ?>
+		 <!-- Sofern es keinen Beitrag gibt, darauf hinweisen: -->
+		 <?php else : ?>
+		  <p>Es gibt keine Beitraege!</p>
+		<?php endif; ?>
 
- <!-- Sofern es keinen Beitrag gibt, darauf hinweisen: -->
- <?php else : ?>
-  <p>Es gibt keine Beitraege!</p>
-<?php endif; ?>
+		<?php wp_footer(); ?>
+
+</body>
+</html>
